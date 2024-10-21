c4a56fc5513fd5eda627e11c47941fddf5053cf5
ahmet.cakar <ahmet.cakar@DESKTOP-B6HJKHB>
2024-10-21 12:59:40 +0300

    boyutlar düzenlendi yeni xaml eklendi

---
+++ Palet_Programlama/Sayfalar/Dizilim_Yap.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Palet_Programlama.Sayfalar
+{
+    /// <summary>
+    /// Interaction logic for Dizilim_Yap.xaml
+    /// </summary>
+    public partial class Dizilim_Yap : Page
+    {
+        public Dizilim_Yap()
+        {
+            InitializeComponent();
+        }
+    }
+}

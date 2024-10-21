48319fedc1b0f365dba64172d45501e31c88f85f
yunuskr <141025570+yunuskr@users.noreply.github.com>
2024-10-21 08:53:51 +0300

    Kullanıcı Giriş Sayfası Eklendi

---
+++ Palet_Programlama/Sayfalar/Kullanici.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Palet_Programlama.Sayfalar
+{
+    /// <summary>
+    /// Interaction logic for Kullanici.xaml
+    /// </summary>
+    public partial class Kullanici : Page
+    {
+        public Kullanici()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateKullaniciPlaceholder();
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateKullaniciPlaceholder();
+        }
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UpdatePasswordPlaceholder();
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdatePasswordPlaceholder();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            UpdatePasswordPlaceholder();
+        }
+
+        private void UpdatePasswordPlaceholder()
+        {
+            // Eğer şifre boşsa, placeholder'ı göster
+            if (string.IsNullOrEmpty(passwordBox.Password))
+            {
+                passwordPlaceholder.Visibility = Visibility.Visible; // Placeholder görünür
+            }
+            else
+            {
+                passwordPlaceholder.Visibility = Visibility.Collapsed; // Placeholder gizlenir
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateKullaniciPlaceholder();
+        }
+        private void UpdateKullaniciPlaceholder()
+        {
+            if (string.IsNullOrEmpty(kullanici_textbox.Text))
+            {
+                KullaniciPlaceholder.Visibility = Visibility.Visible; // Placeholder görünür
+            }
+            else
+            {
+                KullaniciPlaceholder.Visibility = Visibility.Collapsed; // Placeholder gizlenir
+            }
+        }
+
+        private void giris_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

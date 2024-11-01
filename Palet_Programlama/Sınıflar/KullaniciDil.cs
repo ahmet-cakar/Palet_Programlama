88beb180e5e02534fe6aedffd44511714bbc44a5
ahmet.cakar <ahmet.cakar@DESKTOP-B6HJKHB>
2024-11-01 10:42:38 +0300

    guncelleme

--- Palet_Programlama/Sınıflar/KullaniciDil.cs
+++ Palet_Programlama/Sınıflar/KullaniciDil.cs
@@ -9,6 +9,6 @@ namespace Palet_Programlama
     public static class KullaniciDil
     {
        public static  string Kullaniciadi { get; set;}
-        public static string Dil { get; set;}
+       public static string Dil { get; set;}
     }
 }
